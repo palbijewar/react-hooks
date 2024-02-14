d22867c8ef21d260c19eee4dc0137c44c4c83d4c
Pal Bijewar <pal.bijewar@gmail.com>
2024-02-14 16:19:28 +0530

    counter 3 and 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
+import HookCounterFour from "./components/HookCounterFour";
+import HookCounterThree from "./components/HookCounterThree";
 import HookCounterTwo from "./components/HookCounterTwo";
 
 function App() {
   return (
     <>
+    <HookCounterFour />
+    {/* <HookCounterThree /> */}
       {/* <ClassCounter />
       <HookCounter /> */}
-      <HookCounterTwo />
+      {/* <HookCounterTwo /> */}
     </>
   );
 }
